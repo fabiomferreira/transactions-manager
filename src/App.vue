091bc182117ab6302a01e32421a2f332c75e4d16
Fabio Ferreira <fabiomf@protonmail.com>
2019-09-26 22:48:00 -0400

    Add vuetify and new page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app class="header-gradient">
+      <v-toolbar-title class="headline text-uppercase white--text">
+        <span>Transaction Manager</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+$color-base: #159ce4;
+$color-base-purple: #bc85f7;
+$color-base-pink: #e178d7;
+$color-button: #e078d6;
+
+$gradient: linear-gradient(to right, $color-base 0%, $color-base-purple 50%, $color-base-purple 51%, $color-base-pink 100%);
+
+.header-gradient {
+  background: $gradient
 }
 </style>
---
+++ src/views/TransactionRegistration.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>teste</div>
+</template>
+
+<script>
+export default {
+    name: 'TransactionRegistration'
+}
+</script>
+
+<style>
+
+</style>
